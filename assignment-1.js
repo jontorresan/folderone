fd567f1e75fb25e4c8d848e27b6b605155009b81
Jon Torresan <jontorresan94@gmail.com>
2018-08-27 11:34:18 -0700

    second commit

---
+++ assignment-1.js
@@ -0,0 +1,12 @@
+var numbers = []
+for (var i=0;i<10;i++){
+    numbers.push(i);
+    
+}
+console.log(numbers[0]);
+console.log(numbers[9]);
+var car = {}
+car = {
+    colour: 'black',
+}
+console.log(car.colour);
